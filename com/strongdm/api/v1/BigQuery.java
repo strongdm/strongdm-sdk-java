f32827a982cca9588218f143f8a80001c0bbb6ed
Evan Todd <evan@etodd.io>
2020-01-02 12:02:42 -0800

    driver: add protobuf driver definitions
    Addresses #4841
    
    GitOrigin-RevId: 8abab6b06a85b5bd8095e4e7a61bd7416b65b06b

---
+++ com/strongdm/api/v1/BigQuery.java
@@ -0,0 +1,91 @@
+package com.strongdm.api.v1;
+
+public class BigQuery implements Resource {
+  private String id;
+  // Unique identifier of the Resource.
+  public String getId() {
+    return this.id;
+  }
+  // Unique identifier of the Resource.
+  public BigQuery setId(String in) {
+    this.id = in;
+    return this;
+  }
+
+  private String name;
+  // Unique human-readable name of the Resource.
+  public String getName() {
+    return this.name;
+  }
+  // Unique human-readable name of the Resource.
+  public BigQuery setName(String in) {
+    this.name = in;
+    return this;
+  }
+
+  private boolean healthy;
+  // True if the datasource is reachable and the credentials are valid.
+  public boolean getHealthy() {
+    return this.healthy;
+  }
+  // True if the datasource is reachable and the credentials are valid.
+  public BigQuery setHealthy(boolean in) {
+    this.healthy = in;
+    return this;
+  }
+
+  private String endpoint;
+
+  public String getEndpoint() {
+    return this.endpoint;
+  }
+
+  public BigQuery setEndpoint(String in) {
+    this.endpoint = in;
+    return this;
+  }
+
+  private String privateKey;
+
+  public String getPrivateKey() {
+    return this.privateKey;
+  }
+
+  public BigQuery setPrivateKey(String in) {
+    this.privateKey = in;
+    return this;
+  }
+
+  private String project;
+
+  public String getProject() {
+    return this.project;
+  }
+
+  public BigQuery setProject(String in) {
+    this.project = in;
+    return this;
+  }
+
+  private int portOverride;
+
+  public int getPortOverride() {
+    return this.portOverride;
+  }
+
+  public BigQuery setPortOverride(int in) {
+    this.portOverride = in;
+    return this;
+  }
+
+  private String username;
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public BigQuery setUsername(String in) {
+    this.username = in;
+    return this;
+  }
+}
---
+++ com/strongdm/api/v1/Druid.java
@@ -0,0 +1,91 @@
+package com.strongdm.api.v1;
+
+public class Druid implements Resource {
+  private String id;
+  // Unique identifier of the Resource.
+  public String getId() {
+    return this.id;
+  }
+  // Unique identifier of the Resource.
+  public Druid setId(String in) {
+    this.id = in;
+    return this;
+  }
+
+  private String name;
+  // Unique human-readable name of the Resource.
+  public String getName() {
+    return this.name;
+  }
+  // Unique human-readable name of the Resource.
+  public Druid setName(String in) {
+    this.name = in;
+    return this;
+  }
+
+  private boolean healthy;
+  // True if the datasource is reachable and the credentials are valid.
+  public boolean getHealthy() {
+    return this.healthy;
+  }
+  // True if the datasource is reachable and the credentials are valid.
+  public Druid setHealthy(boolean in) {
+    this.healthy = in;
+    return this;
+  }
+
+  private String hostname;
+
+  public String getHostname() {
+    return this.hostname;
+  }
+
+  public Druid setHostname(String in) {
+    this.hostname = in;
+    return this;
+  }
+
+  private int portOverride;
+
+  public int getPortOverride() {
+    return this.portOverride;
+  }
+
+  public Druid setPortOverride(int in) {
+    this.portOverride = in;
+    return this;
+  }
+
+  private String username;
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public Druid setUsername(String in) {
+    this.username = in;
+    return this;
+  }
+
+  private String password;
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public Druid setPassword(String in) {
+    this.password = in;
+    return this;
+  }
+
+  private int port;
+
+  public int getPort() {
+    return this.port;
+  }
+
+  public Druid setPort(int in) {
+    this.port = in;
+    return this;
+  }
+}
