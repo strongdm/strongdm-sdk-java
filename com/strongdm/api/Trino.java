7331bae980987ade11ff673f70ee32e8212f1606
SupportSDM <support@strongdm.com>
2025-06-20 13:14:57 +0000

    14.25.0

--- com/strongdm/api/Trino.java
+++ com/strongdm/api/Trino.java
@@ -17,10 +17,6 @@
 
 package com.strongdm.api;
 
-/**
- * Trino is currently unstable, and its API may change, or it may be removed, without a major
- * version bump.
- */
 public class Trino implements Resource {
   private String bindInterface;
   /**
