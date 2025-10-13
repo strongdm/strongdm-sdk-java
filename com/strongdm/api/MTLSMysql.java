e882fa4905f11c001c84525f8661c54e197df0c3
SupportSDM <support@strongdm.com>
2025-10-13 09:01:24 +0000

    15.22.0

--- com/strongdm/api/MTLSMysql.java
+++ com/strongdm/api/MTLSMysql.java
@@ -17,10 +17,6 @@
 
 package com.strongdm.api;
 
-/**
- * MTLSMysql is currently unstable, and its API may change, or it may be removed, without a major
- * version bump.
- */
 public class MTLSMysql implements Resource {
   private String bindInterface;
   /**
