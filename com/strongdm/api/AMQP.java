0ed36ae617780f4c9a1c1d8300f91e6553d945e7
SupportSDM <support@strongdm.com>
2026-02-25 16:52:36 +0000

    16.11.0

--- com/strongdm/api/AMQP.java
+++ com/strongdm/api/AMQP.java
@@ -17,10 +17,6 @@
 
 package com.strongdm.api;
 
-/**
- * AMQP is currently unstable, and its API may change, or it may be removed, without a major version
- * bump.
- */
 public class AMQP implements Resource {
   private String bindInterface;
   /**
