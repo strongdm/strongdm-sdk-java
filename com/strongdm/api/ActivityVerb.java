7bf9ceb2e034dcb671809099daeebcf861500482
SupportSDM <support@strongdm.com>
2026-02-27 11:47:50 +0000

    16.14.0

--- com/strongdm/api/ActivityVerb.java
+++ com/strongdm/api/ActivityVerb.java
@@ -253,6 +253,7 @@ public final class ActivityVerb {
   public static final String SUSPENDED_USER_ENROLL_ATTEMPT_FROM_THE_UI =
       "attempt to enroll by a suspended user from the Admin UI";
   public static final String RESOURCE_LOCKED = "user locked a resource";
+  public static final String RESOURCE_LOCK_EXTENDED = "user extended a resource lock";
   public static final String RESOURCE_UNLOCKED = "user unlocked a resource";
   public static final String RESOURCE_FORCE_UNLOCKED = "admin force-unlocked a resource";
   public static final String RESOURCE_LOCK_REJECTED = "user lock rejected for a resource";
